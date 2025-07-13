240de9f95fb9b40fc77612a3268a5783c6e851b7
Aniket shinde <103438508+aniket162002@users.noreply.github.com>
2025-07-13 21:54:43 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import localFont from 'next/font/local';
 const inter = localFont({
   src: [
     {
-      path: '/fonts/Inter-Regular.woff2', // Retyped to ensure no hidden characters
+     path: './public/fonts/Inter-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
